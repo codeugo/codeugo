08f977fc9b06884c4edf33186cec5d26b8bbca6d
Askehraz <virolle.u@gmail.com>
2023-02-17 13:17:36 +0100

    6

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,12 +1,17 @@
 <template>
     <header class="sticky top-0 bg-weather-primary shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-2">
-            <RouterLink :to="{name: 'home'}">
-                <div class="flex items-center gap-3">
-                    <img class="text-2xl" src="../assets/code(ugo).svg">
-                    <p class="text-2xl">Projets</p>
-                </div>
-            </RouterLink>
+            <div class="flex items-center gap-3">
+                <RouterLink :to="{name: 'home'}">
+                    <div class="flex items-center gap-3">
+                        <img src="../assets/code(ugo).svg">
+                    </div>
+                </RouterLink>
+                <p class="text-2xl">Projets</p>
+            </div>
+            <div class="flex gap-3 flex-1 justify-end">
+                <img class="text-white" src="../assets/sun.svg">
+            </div>
         </nav>
     </header>
 </template>
