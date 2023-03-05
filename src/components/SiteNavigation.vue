fc09b7693010ce5aa9682c66b779416b2c8b5855
Askehraz <virolle.u@gmail.com>
2023-03-05 23:09:02 +0100

    10

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,13 +1,14 @@
 <template>
     <header class="sticky top-0 bg-weather-primary shadow-lg">
-        <nav class="container flex flex-col sm:flex-row items-center gap-6 text-white py-2 font-mplusrounded">
+        <nav class="container flex flex-col sm:flex-row justify-center items-center gap-6 text-white py-2 font-mplusrounded">
             <RouterLink :to="{name: 'home'}">
                 <div class="flex gap-3">
                     <img src="../assets/code(ugo).svg">
                 </div>
             </RouterLink>
             <p>Projets</p>
-            <a href="https://github.com/Askehraz" class="button">
+            <a href="https://github.com/Askehraz" class="inline-flex gap-2 button">
+                <img src="../assets/github.svg">
                 Github
             </a>
             <div class="flex justify-end flex-1">
